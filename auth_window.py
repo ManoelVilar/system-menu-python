56404b925b0e1063b3a49e029af0590ea1e0103b
ManoelVilar <manoelvilarcontato@gmail.com>
2026-03-15 23:57:46 -0300

    Create auth_window.py with authentication window classes.

---
+++ auth_window.py
@@ -0,0 +1,27 @@
+class AuthWindow:
+    def __init__(self):
+        pass  # Initialize authentication window.
+
+class Login:
+    def __init__(self):
+        self.username = ""
+        self.password = ""
+    
+    def authenticate(self):
+        pass  # Logic for user authentication goes here.
+
+class PasswordReset:
+    def __init__(self):
+        self.email = ""
+
+    def reset(self):
+        pass  # Logic to reset password goes here.
+
+class AccountCreation:
+    def __init__(self):
+        self.username = ""
+        self.password = ""
+        self.email = ""
+
+    def create_account(self):
+        pass  # Logic to create a new account goes here.
